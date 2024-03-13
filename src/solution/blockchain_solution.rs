48ae7df8268c2e90c69c6f0bc6150021b9dc274f
Samuel Skean <sskean2@uic.edu>
2024-03-13 05:17:35 -0500

    Specified API as best I could to be the same as a1, but for Rust.
    Minus the audit methods, though.

--- src/solution/blockchain_solution.rs
+++ src/solution/blockchain_solution.rs
@@ -1,17 +1,42 @@
 use super::coin_solution::CoinSolution;
 use super::wallet_solution::WalletSolution;
 use std::collections::HashSet;
+use std::rc::Rc;
 
 struct BlockchainSolution {
     all_wallets: HashSet<WalletSolution>,
 }
 
 impl BlockchainSolution {
-    fn attempt_coins_removal(
-        &mut self,
-        wallet: WalletSolution,
-        coins: HashSet<CoinSolution>,
-    ) -> bool {
-        false
+    fn new() -> BlockchainSolution {
+        unimplemented!()
+    }
+
+    fn create_wallet(capacity: i32) -> Rc<WalletSolution> {
+        unimplemented!()
+    }
+
+    fn create_coin(id: i32) -> Rc<CoinSolution> {
+        unimplemented!()
+    }
+
+    fn add_coins(wallet: &mut WalletSolution, coins: &HashSet<CoinSolution>) -> bool {
+        unimplemented!()
+    }
+
+    fn transfer_coins(from: &mut WalletSolution, to: &mut WalletSolution, coins: &HashSet<CoinSolution>) -> bool {
+        unimplemented!()
+    }
+
+    fn pay_rent(wallet: &mut WalletSolution, coins: &HashSet<CoinSolution>) -> bool {
+        unimplemented!()
+    }
+
+    fn redeem_coins(wallet: &mut WalletSolution, coins: &HashSet<CoinSolution>) -> bool {
+        unimplemented!()
+    }
+
+    fn get_coins() -> HashSet<CoinSolution> {
+        unimplemented!()
     }
 }
